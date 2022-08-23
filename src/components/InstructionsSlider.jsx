be5455918d600eb2d54e693a9bfc10412361a4ce
ArtemSmirnovHSE <ar.smirnov2015@mail.ru>
2022-08-23 19:38:42 +0300

    Last slider change, i hope

--- src/components/InstructionsSlider.jsx
+++ src/components/InstructionsSlider.jsx
@@ -5,26 +5,30 @@ const InstructionsSlider = () => {
 
   return ( <div className='home-details'>
     
-    <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+    <div id="carouselInstruction" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="/instruction_1.png" className="d-block w-100" alt="..."/>
+      <img src="/instruction_1.png" className="d-block w-100" alt="Лицей НИУ ВШЭ Адаптация Инструкция 1"/>
     </div>
     <div className="carousel-item">
-      <img src="/instruction_2.png" className="d-block w-100" alt="..."/>
-
+      <img src="/instruction_2.png" className="d-block w-100" alt="Лицей НИУ ВШЭ Адаптация Инструкция 2"/>
     </div>
     <div className="carousel-item">
-      <img src="/instruction_3.png" className="d-block w-100" alt="..."/>
-
+      <img src="/instruction_3.png" className="d-block w-100" alt="Лицей НИУ ВШЭ Адаптация Инструкция 3"/>
     </div>
-
     <div className="carousel-item">
-      <img src="/instruction_4.png" className="d-block w-100" alt="..."/>
-
+      <img src="/instruction_4.png" className="d-block w-100" alt="Лицей НИУ ВШЭ Адаптация Инструкция 4"/>
     </div>
 
   </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselInstruction" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselInstruction" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
 </div>
     </div>
 
